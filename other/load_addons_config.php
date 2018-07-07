4d0e82f699bf9eb2c02c4f4afa8255520480f504
DanielR92 <DanielR92@users.noreply.github.com>
2018-07-07 13:19:11 +0200

    Add files via upload

--- other/load_addons_config.php
+++ other/load_addons_config.php
@@ -11,4 +11,30 @@ function load_addons_config($mysqlcon,$lang,$dbname,$timezone,$logpath) {
 	}
 	//$addons_config['assign_groups_groupids']['value'];
 }
+
+function get_plugins_db($mysqlcon,$dbname,$timezone,$logpath, $addon) 
+{
+	if(!isset($mysqlcon) || $mysqlcon == NULL || ($addons_config = $mysqlcon->query("SELECT * FROM `$dbname`.`plugin_$addon`")) === false) {
+		if(function_exists('enter_logfile')) { 
+			enter_logfile($logpath,$timezone,2,"Error on loading plugins config.. Database down, not reachable, corrupt or empty?");
+		} else {
+			echo "Error on loading plugins config.. Database down, not reachable, corrupt or empty?";
+		}
+	} else {
+		return $addons_config->fetchAll();
+	}
+}
+
+function get_plugins_status($mysqlcon,$dbname,$timezone,$logpath, $addon) 
+{
+	if(!isset($mysqlcon) || $mysqlcon == NULL || ($addons_config = $mysqlcon->query("SELECT enable FROM `$dbname`.`addon_job` WHERE module='$addon'")) === false) {
+		if(function_exists('enter_logfile')) { 
+			enter_logfile($logpath,$timezone,2,"Error on loading plugins config.. Database down, not reachable, corrupt or empty?");
+		} else {
+			echo "Error on loading plugins config.. Database down, not reachable, corrupt or empty?";
+		}
+	} else {
+		return $addons_config->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
 ?>
